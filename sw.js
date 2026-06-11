114a3e55e1feb44310bbd2bc840a6f9ea8b0cf28
井上太晴 <inouetaisei@inouefutoshiharenoMacBook-Pro.local>
2026-06-11 11:34:54 +0900

    Bust service worker cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cal-alarm-v1';
+const CACHE = 'cal-alarm-v3';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
